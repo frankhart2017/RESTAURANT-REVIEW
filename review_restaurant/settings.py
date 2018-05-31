00ffa896002dfdecce3d076fdae769983f8b6760
frankhart2017 <sdharchou@gmail.com>
2018-05-31 17:49:24 +0530

    Change settings.py

--- review_restaurant/settings.py
+++ review_restaurant/settings.py
@@ -119,6 +119,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
 STATIC_URL = '/static/'
